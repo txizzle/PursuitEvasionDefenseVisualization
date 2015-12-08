4b8b75652b969ccf3a5d343b553b450a7385061f
Ted Xiao <t.xiao@berkeley.edu>
2015-12-08 01:05:31 -0800

    Adding index.html and updating contours.js

--- js/contours.js
+++ js/contours.js
@@ -208,22 +208,54 @@ function updateContour() {
   if (leaveTrails == 0) {
     d3.selectAll("path").remove();
   }
+  d3.selectAll("circle").remove();
 //  if (islands.indexOf(key) > -1) {
-    d3.csv(currFile + "1.csv", function(mydata) {
-      svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
-        line);
-    });
-    d3.csv(currFile + "2.csv", function(mydata) {
-      svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
-        line);
-    });
+  d3.csv(currFile + "1.csv", function(mydata) {
+    svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
+      line);
+  });
+  d3.csv(currFile + "2.csv", function(mydata) {
+    svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
+      line);
+  });
 ////  } else {
 //    console.log("One island!");
-    d3.csv(currFile + ".csv", function(mydata) {
-      svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
-        line);
-    });
+  d3.csv(currFile + ".csv", function(mydata) {
+    svg.append("path").datum(mydata).attr("class", "mainline").attr("d",
+      line);
+  });
 //  }
+
+  //Plot evader path
+  svg.append("path").datum([
+    [-0.6, -0.8],
+    [-0.6, 0.8],
+    [0.6, 0.8],
+    [0.6, -0.8],
+    [-0.6, -0.8]
+  ]).attr("class", "evaderpath").attr("d", objectLine).attr(
+    "stroke-dasharray", "10 5");
+  
+  //Plot evader
+  evader = svg.append("circle").datum([evaderX, evaderY]).attr("cx", function(
+    d) {
+    return exScale(d[0]);
+  }).attr("cy", function(d) {
+    return eyScale(d[1]);
+  }).attr("r", 5).attr("fill", "blue").call(drag);
+  
+  //Plot catch radius
+  catch_radius = svg.append("circle").datum([evaderX, evaderY]).attr("cx",
+    function(d) {
+      return exScale(d[0]);
+    }).attr("cy", function(d) {
+    return eyScale(d[1]);
+  }).attr("r", height / 20).attr("fill", "none").attr("stroke", "black").attr(
+    "stroke-width", "3px").attr("stroke-dasharray", "10 5").call(drag);
+  
+  //Plot obstacle
+ svg.append("path").datum(getObstacle(parseInt(currTime))).attr("class",
+    "obstacle").attr("d", objectLine);
 };
 
 function updateTime(t) {
@@ -232,6 +264,63 @@ function updateTime(t) {
   document.getElementById('timeSlider').value = t;
   updateContour();
 }
+
+function getValfromXY(x, y) {
+  var s;
+  if (x == 0.6) {
+    // 1.2 < s < 2.8
+    s = y + 2;
+  } else if (x == -0.6) {
+    // 4 < s < 5.6
+    s = 4.8 - y;
+  } else {
+    if (y == 0.8) { //top
+      // 1.2 < s < 2.8
+      s = 3.4 - x;
+    } else if (y == -0.8) { //bottom
+      // 4 < s < 5.6
+      s = x + 0.6;
+    }
+  }
+  return Math.round(s * 85 / 5.6 + 1);
+}
+
+function getEvaderY(val) {
+  var s = (val - 1) / 85 * 5.6;
+  if (s < 1.2) return -0.8;
+  else if (s < 2.8) return -0.8 + (s - 1.2);
+  else if (s < 4.0) return 0.8;
+  else return 0.8 - (s - 4.0);
+}
+
+function getEvaderX(val) {
+  var s = (val - 1) / 85 * 5.6;
+  if (s < 1.2) return s - 0.6;
+  else if (s < 2.8) return 0.6;
+  else if (s < 4.0) return 0.6 - (s - 2.8);
+  else return -0.6;
+}
+
+function getObstacle(time) {
+  var t = time / 175.0;
+  if (t >= 0 && t < 0.2) {
+    var bt = -2 * t;
+  } else if (t < 0.6) {
+    var bt = -0.4;
+  } else if (t < 0.8) {
+    var bt = -2.8 + 4 * t;
+  } else {
+    var bt = 0.4;
+  }
+  return ([
+    [-0.2, -0.6 + bt],
+    [-0.2, 0.6 + bt],
+    [0.2, 0.6 + bt],
+    [0.2, -0.6 + bt]
+  ]);
+}
+
+
 function updateEvader(v) {
   currEvader = v.toString();
   $('#evaderLabel').val(v);
